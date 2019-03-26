50b1bbb9ccc1b2fb562228a134f66c09d98c5b05
Udhay Hari <udhay@kabadiwallaconnect.in>
2019-03-26 10:16:37 +0530

    Header completed

---
+++ seed-without-redux/src/containers/App/MainWrapper.jsx
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class MainWrapper extends PureComponent {
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+  };
+
+  render() {
+    return (
+      <div className="theme-light">
+        <div className="wrapper">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainWrapper;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    activeLink: null
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="header">
+        <div className="text-container">
+          <h2>
+            <span className="header-text--primary">Udhay hari</span>
+            <span className="header-text--secondary">front end developer</span>
+          </h2>
+            <a class="button white--button" href="/" >
+              Projects
+            </a>  
+        </div>  
       </div>
     );
   }
